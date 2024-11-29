d89207d395676d13e40148e418b1b83183d26dd2
Liudmyla Lysenko <lysenko.liudmila.a@gmail.com>
2024-11-29 22:41:08 +0100

    feat(flashcards): replace div with textarea to edit purposes

--- src/features/flashcards/components/flashcard-sets/flashcard-list-item.tsx
+++ src/features/flashcards/components/flashcard-sets/flashcard-list-item.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@nextui-org/react';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
+
+import AppTooltip from '@/components/app-tooltip/app-tooltip';
 
 import { updateFlashcard } from '@/features/flashcards/actions';
 
@@ -15,7 +17,22 @@ export default function FlashcardListItem({
   card: Flashcard;
   handleStarredCount: (isStarred: boolean) => void;
 }) {
-  const [isFlashcardStarred, setIsFlashcardStarred] = useState(card.isStarred);
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [isFlashcardStarred, setIsFlashcardStarred] = useState<boolean>(card.isStarred);
+  const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
+  const termRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useLayoutEffect(() => {
+    if (termRef && termRef.current) {
+      termRef.current.style.height = `${termRef?.current?.scrollHeight}px`;
+    }
+  }, [termRef.current?.scrollHeight]);
+
+  useLayoutEffect(() => {
+    if (descriptionRef && descriptionRef.current) {
+      descriptionRef.current.style.height = `${descriptionRef?.current?.scrollHeight}px`;
+    }
+  }, [descriptionRef.current?.scrollHeight]);
 
   async function toggleStarred() {
     setIsFlashcardStarred(!isFlashcardStarred);
@@ -25,39 +42,75 @@ export default function FlashcardListItem({
     handleStarredCount(updatedFlashcard.isStarred);
   }
 
+  function toggleEditMode() {
+    setIsEditMode(!isEditMode);
+  }
+
   return (
     <div
       key={card.id}
       className={clsx(styles.shadow, 'flex min-h-[3.625rem] justify-between rounded bg-white px-6 py-5')}>
       <div className="flex w-full gap-2">
-        <div className={clsx(card.definition && 'border-r-2 border-ghost-white', 'w-1/3 py-2 pr-4')}>{card.term}</div>
-        <div className="flex-1 py-2 pl-8 pr-4">{card.definition}</div>
+        <textarea
+          ref={termRef}
+          readOnly={true}
+          value={card.term}
+          className={clsx(
+            card.definition && 'border-r-2 border-ghost-white',
+            'h-auto w-1/3 resize-none overflow-hidden py-2 pr-4'
+          )}>
+          {card.term}
+        </textarea>
+        <textarea
+          ref={descriptionRef}
+          readOnly={true}
+          value={card.definition}
+          className="h-auto flex-1 resize-none overflow-hidden py-2 pl-8 pr-4">
+          {card.definition}
+        </textarea>
       </div>
       <div className="flex w-[6.75rem]">
-        <Button
-          isIconOnly
-          aria-label="Star the card"
-          radius="full"
-          className="bg-transparent hover:bg-bright-gray"
-          onClick={toggleStarred}>
-          <Image
-            src="/static/images/icon__star.svg"
-            alt="Star the card"
-            width={24}
-            height={24}
-            className={clsx(isFlashcardStarred && styles.active)}
-          />
-        </Button>
-        <Button
-          isIconOnly
-          aria-label="Play the card sound"
-          radius="full"
-          className="bg-transparent hover:bg-bright-gray">
-          <Image src="/static/images/icon__sound.svg" alt="Play the card sound" width={24} height={24} />
-        </Button>
-        <Button isIconOnly aria-label="Edit" radius="full" className="bg-transparent hover:bg-bright-gray">
-          <Image src="/static/images/icon__pencil.svg" alt="Edit the card" width={24} height={24} />
-        </Button>
+        <AppTooltip content="Star">
+          <Button
+            isIconOnly
+            aria-label="Star the card"
+            radius="full"
+            className="bg-transparent hover:bg-bright-gray"
+            onClick={toggleStarred}>
+            <Image
+              src="/static/images/icon__star.svg"
+              alt="Star the card"
+              width={24}
+              height={24}
+              className={clsx(isFlashcardStarred && styles.active)}
+            />
+          </Button>
+        </AppTooltip>
+        <AppTooltip content="Play audio for this term">
+          <Button
+            isIconOnly
+            aria-label="Play the card sound"
+            radius="full"
+            className="bg-transparent hover:bg-bright-gray">
+            <Image src="/static/images/icon__sound.svg" alt="Play the card sound" width={24} height={24} />
+          </Button>
+        </AppTooltip>
+        <AppTooltip content="Edit">
+          <Button
+            isIconOnly
+            aria-label="Edit"
+            radius="full"
+            className="bg-transparent hover:bg-bright-gray"
+            onClick={toggleEditMode}>
+            <Image
+              src="/static/images/icon__pencil.svg"
+              alt="Edit the card"
+              width={24}
+              height={24}
+              className={clsx(isEditMode && styles.active)}
+            />
+          </Button>
+        </AppTooltip>
       </div>
     </div>
   );
