3cfdf31d754f4c293ab7c4f069b596f3c5e9c6d9
Liudmyla Lysenko <lysenko.liudmila.a@gmail.com>
2024-12-19 22:49:31 +0100

    feat(folders): add dropdown to add set to folders

--- src/features/flashcards/components/flashcard-sets/flashcard-set-actions.tsx
+++ src/features/flashcards/components/flashcard-sets/flashcard-set-actions.tsx
@@ -1,11 +1,27 @@
 'use client';
 
 import { Flashcard, FlashcardSet, User } from '.prisma/client';
-import { Button } from '@nextui-org/react';
-import Image from 'next/image';
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, user } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 import AppTooltip from '@/components/app-tooltip/app-tooltip';
+
+type MenuItem = {
+  imagePath: string;
+  key: string;
+  label: string;
+  href?: string;
+};
+
+const menuItems: MenuItem[] = [
+  {
+    label: 'Add to folder',
+    key: 'addToFolder',
+    href: '/create-set',
+    imagePath: 'icon__folder.svg'
+  }
+];
 
 type Props = {
   set: Partial<FlashcardSet & { flashcards?: Flashcard[] } & { user: Partial<User> }> & { starred: number };
@@ -16,6 +32,10 @@ export default function FlashcardSetActions({ set }: Props) {
 
   function redirectToEditPage() {
     router.push(`/${set.id}/edit`);
+  }
+
+  function handleMenuItemClick(item: MenuItem) {
+    alert('Not implemented yet!');
   }
 
   return (
@@ -35,16 +55,34 @@ export default function FlashcardSetActions({ set }: Props) {
           <Image src="/images/icon__pencil.svg" alt="Edit the card" width={24} height={24} />
         </Button>
       </AppTooltip>
-      <AppTooltip content="More" placement="bottom">
-        <Button
-          isIconOnly
-          aria-label="More"
-          variant="ghost"
-          radius="sm"
-          className="bg-transparent hover:bg-bright-gray">
-          <Image src="/images/icon__more.svg" alt="More" width={24} height={24} />
-        </Button>
-      </AppTooltip>
+
+      <Dropdown placement="bottom-end">
+        <DropdownTrigger>
+          <Button
+            isIconOnly
+            aria-label="More"
+            variant="ghost"
+            radius="sm"
+            className="bg-transparent hover:bg-bright-gray">
+            <Image src="/images/icon__more.svg" alt="More" width={24} height={24} />
+          </Button>
+        </DropdownTrigger>
+
+        <DropdownMenu aria-label="Edit Set Actions" variant="flat" items={menuItems}>
+          {item => (
+            <DropdownItem
+              key={item.label}
+              textValue={item.label}
+              className="text-dark-electric-blue"
+              onPress={() => handleMenuItemClick(item)}>
+              <div className="flex items-center gap-3">
+                <Image src={`/images/${item.imagePath}`} alt={item.label} width={24} height={24} className="size-5" />
+                <p>{item.label}</p>
+              </div>
+            </DropdownItem>
+          )}
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 }
